5a7ebd749fc352a1fb1f497beba40442b4b6729d
zachansell <89551318+zachansell@users.noreply.github.com>
2021-08-31 11:33:33 -0700

    implemented deposit method

--- src/ATM.java
+++ src/ATM.java
@@ -4,5 +4,15 @@ public class ATM {
 	public void deposit (String account, double amount)
 	{
 		
+		if(!accounts.contains(new Account(account)))
+		{
+			Account newAcc = new Account(account);
+			accounts.add(newAcc);
+			newAcc.deposit(amount);
+		}
+		else
+		{
+			accounts.get(accounts.indexOf(new Account(account))).deposit(amount);
+		}
 	}
 }
